05997f9ee6b986dda809ed40e2954ba1f06df0d2
kacode357 <kacode357@gmail.com>
2026-02-28 20:55:39 +0700

    Initial commit

--- fe_ui/src/main.jsx
+++ fe_ui/src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { createRoot } from 'react-dom/client'
 import { ConfigProvider, Layout } from 'antd'
 import { RouterProvider, createBrowserRouter, Outlet } from 'react-router-dom'
